4b3f8f294f24cd0b636673f3e5be849815ffc919
Hulunlante Worku <hulunlante.w@gmail.com>
2023-10-06 22:33:44 +0300

    soil test added

---
+++ src/models/sample/soilTest.js
@@ -0,0 +1,77 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/db_config");
+const Tank = require("./tank");
+
+const SoilTest = sequelize.define(
+  "soilTest",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    soilType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    soilNature: {
+      type: DataTypes.ENUM,
+      values: ["Heavy","Medium","Light"]
+      allowNull: false,
+    },
+    soilPh: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    organicCarbon: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    availableNitrogen: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    prosperous: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    potash: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    sulphur: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    zinc: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    iron: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    boran: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    limeTest: {
+      type: DataTypes.ENUM,
+      values:["Light","Medium","Heavy"]
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: ["1", "2"],
+      defaultValue: "1",
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
+
+Tank.hasMany(SoilTest);
+SoilTest.belongsTo(Tank);
+
+module.exports = SoilTest;
---
+++ src/models/sample/waterTest.js
@@ -0,0 +1,83 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/db_config");
+const Tank = require("./tank");
+
+const WaterTest = sequelize.define(
+  "WaterTest",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    ph: {
+      type: DataTypes.DECIMAL,
+    },
+    temprature: {
+      type: DataTypes.DECIMAL,
+    },
+    co3: {
+      type: DataTypes.DECIMAL,
+    },
+    hco3: {
+      type: DataTypes.DECIMAL,
+    },
+    totalAlkanility: {
+      type: DataTypes.DECIMAL,
+    },
+    totalHardness: {
+      type: DataTypes.DECIMAL,
+    },
+    ca: {
+      type: DataTypes.DECIMAL,
+    },
+    mg: {
+      type: DataTypes.DECIMAL,
+    },
+    k: {
+      type: DataTypes.DECIMAL,
+    },
+    fe: {
+      type: DataTypes.DECIMAL,
+    },
+    na: {
+      type: DataTypes.DECIMAL,
+    },
+    cl2: {
+      type: DataTypes.DECIMAL,
+    },
+    tan: {
+      type: DataTypes.DECIMAL,
+    },
+    nh3: {
+      type: DataTypes.DECIMAL,
+    },
+    no2: {
+      type: DataTypes.DECIMAL,
+    },
+    h2s: {
+      type: DataTypes.DECIMAL,
+    },
+    co2: {
+      type: DataTypes.DECIMAL,
+    },
+    dissolvedOxygen: {
+      type: DataTypes.DECIMAL,
+    },
+    electricalConductivity: {
+      type: DataTypes.DECIMAL,
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: ["1", "2"],
+      defaultValue: "1",
+    },
+  },
+  { freezeTableName: true }
+);
+
+Tank.hasMany(WaterTest);
+WaterTest.belongsTo(Tank);
+
+module.exports = WaterTest;
