f7f312f0db012e0f1cbcbd05c1770f3078442b49
HuluWZ <hulunlante.w@gmail.com>
2023-12-31 20:47:24 +0300

    Update API route in index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -45,7 +45,7 @@ app.use("/api/plankton", planktonTestRoutes);
 app.use("/api/culture", cultureTestRoutes);
 app.use("/api/test", allTestRoutes);
 app.use("/api/lab", labRoutes);
-// app.use("/api", stateDistrictRoutes);
+app.use("/api/location", stateDistrictRoutes);
 
 module.exports = app; 
 
